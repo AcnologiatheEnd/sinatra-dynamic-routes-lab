cf25973522b51623f7693046941c9f329da80be9
Winston  Chung <18.chung.winston@ctkprep.org>
2019-12-23 13:15:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ class App < Sinatra::Base
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     
-    @ans = @number1 @operation @number2
+    @ans = @number1.@operation(@number2)
     "#{@ans}"
   end
 end
