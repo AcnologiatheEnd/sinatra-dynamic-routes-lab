dcc83d0acbf808a65e3528cd1fd71d4b53a7c172
Winston  Chung <18.chung.winston@ctkprep.org>
2019-12-23 13:03:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name' do
+   new_name = params[:name].reverse
+    "#{new_name}"
+  end
+  
+  get '/square/:number' do
+    new_num = (params[:number].to_i ** 2).to_s
+    "#{new_num}"
+  end
+  
+  get '/say/:number/:phrase' do
+    str = ""
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @num.times { str += "#{@phrase}\n" }
+    str
+  end
+  
+  get 'say/:word/'
 end
